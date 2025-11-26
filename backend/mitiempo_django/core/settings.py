da2483bd9c1e285faa4eba36f6c7f740e4e8910f
MattiasFigueroa <mathyaz77@gmail.com>
2025-11-25 23:57:59 -0300

    cree el archivo modal para recuperar contraseña pero esta vacio todavia

--- backend/mitiempo_django/core/settings.py
+++ backend/mitiempo_django/core/settings.py
@@ -144,11 +144,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME':'mitiempo',
+        'NAME':'mitiempo3',
         'USER': 'root',
         'PASSWORD' : 'root',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
